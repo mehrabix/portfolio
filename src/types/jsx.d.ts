3b16de7a7e05e950d4a4577b85fb73be97bebd1c
mehrabix <mehrabi@post.com>
2025-04-01 02:04:44 +0330

    Refactor JSX type definitions: update HTMLAttributes to omit ThreeElements and remove deprecated react-three-fiber type definitions for improved type safety and clarity.

--- src/types/jsx.d.ts
+++ src/types/jsx.d.ts
@@ -1,13 +1,12 @@
 import { ThreeElements } from '@react-three/fiber'
+import { HTMLAttributes } from 'react'
 
 declare module 'react' {
-  interface HTMLAttributes<T> extends ThreeElements {}
+  interface HTMLAttributes<T> extends Omit<HTMLAttributes<T>, keyof ThreeElements> {}
 }
 
 declare global {
   namespace JSX {
-    interface IntrinsicElements {
-      [elemName: string]: any;
-    }
+    interface IntrinsicElements extends ThreeElements {}
   }
 } 
